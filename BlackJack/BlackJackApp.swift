132579b25a1f6780f1f227e036769f7baa4d9e01
Rosenfield <rosenfield.school@gmail.com>
2024-08-25 12:28:09 +0700

    Initial Commit

---
+++ BlackJack/BlackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackApp.swift
+//  BlackJack
+//
+//  Created by Quan Nguyen Tien Minh on 25/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackJack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlackJack
+//
+//  Created by Quan Nguyen Tien Minh on 25/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
